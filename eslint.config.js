4163653f3816b0fa5ab0d28cd6bfef3930ef57d5
Liam O'Dell <liam.riley.odell@gmail.com>
2026-02-01 20:09:53 -0900

    fix: add 'scripts' to eslint ignore

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
-    ignores: ['dist', 'node_modules', '*.js', '!eslint.config.js'],
+    ignores: ['dist', 'node_modules', '*.js', '!eslint.config.js', 'scripts'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
