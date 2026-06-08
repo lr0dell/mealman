2cac4a8741f06287a45f3d7f80408bbd5dc592d8
Liam O'Dell <liam.riley.odell@gmail.com>
2026-06-07 18:33:24 -0700

    chore: simplify test cmd

--- .mise/tasks/test.bash
+++ .mise/tasks/test.bash
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 #MISE description="run tests (watch mode by default)"
-#USAGE flag "-r" help="run once instead of watching"
 set -o pipefail -o errexit -o nounset
-if [ "${usage_r:-}" = "true" ]; then
-  npx vitest run
-else
-  npx vitest
-fi
+npx vitest "$@"
